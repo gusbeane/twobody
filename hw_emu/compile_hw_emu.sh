4b0825be50fb6e48c2391d6f7d7b152d5c9a0770
Gus Beane <gusbeane@gmail.com>
2022-11-02 16:55:58 -0400

    Add hw_emu

---
+++ hw_emu/compile_hw_emu.sh
@@ -0,0 +1,4 @@
+g++ -Wall -g -std=c++11 ../sw_src/event_timer.cpp ../sw_src/xilinx_ocl_helper.cpp ../sw_src/twobody.cpp -o twobody -I${XILINX_XRT}/include/ -L${XILINX_XRT}/lib/-rdynamic /opt/xilinx/xrt/lib/libxrt_core.so /opt/xilinx/xrt/lib/libxilinxopencl.so -lOpenCL -pthread -lrt -lstdc++
+emconfigutil --platform xilinx_u200_gen3x16_xdma_2_202110_1 --nd 1
+v++ -c -t hw_emu --platform xilinx_u200_gen3x16_xdma_2_202110_1 --config ../u200.cfg -k twobody -I../hw_src ../hw_src/twobody.cpp -o twobody.xo
+v++ -l -t hw_emu --platform xilinx_u200_gen3x16_xdma_2_202110_1 --config ../u200.cfg ./twobody.xo -o twobody.xclbin
